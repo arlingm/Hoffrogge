3dda7b9446d2ba27ef1db5fd9efae6c374d3db49
Kurs <Kurs@172.16.99.127>
2019-04-04 15:30:33 +0200

    Sören ist lustig. Und wir haben den schönen key Listener bearbeitet, und kennengelernt.

--- src/com/hoffrogge/lehreinheit07/DemoKeyListener.java
+++ src/com/hoffrogge/lehreinheit07/DemoKeyListener.java
@@ -2,12 +2,14 @@ package com.hoffrogge.lehreinheit07;
 
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.ArrayList;
 
 import javax.swing.JLabel;
 
 public class DemoKeyListener implements KeyListener {
 
 	private JLabel textLabel;
+	private int zahl1 = 0, zahl2 = 0, ges = 0, pressHaeufigkeit = 0;
 
 	public DemoKeyListener(JLabel textLabel) {
 		this.textLabel = textLabel;
@@ -16,11 +18,43 @@ public class DemoKeyListener implements KeyListener {
 	@Override
 	public void keyPressed(KeyEvent e) {
 
+		int i = 0;
+		ArrayList<String> tolleListe = new ArrayList<String>();
 		gebeTasteAus(e, "gedrückt");
 
+		try {
+			tolleListe.add(String.valueOf(e.getKeyChar()));
+		} catch (NumberFormatException e1) {
+			e1.printStackTrace();
+			System.out.println("hi");
+			System.out.println(String.valueOf(i));
+		}
+
+		// Enter
+		if (KeyEvent.VK_ENTER == e.getKeyCode()) {
+			pressHaeufigkeit++;
+			if (pressHaeufigkeit == 1) {
+				for (int l = 0; l < size(tolleListe); l++) {
+					zahl1 += Integer.parseInt(tolleListe.get(l)) * Math.pow(10, (double) size(tolleListe) - l);
+					System.out.println("Hi");
+				}
+				tolleListe = null;
+			} else if (pressHaeufigkeit == 2) {
+				for (int l = 0; l < size(tolleListe); l++) {
+					zahl1 += Integer.parseInt(tolleListe.get(l)) * Math.pow(10, (double) size(tolleListe) - l);
+				}
+				ges = zahl1 + zahl2;
+				System.out.println(String.valueOf(ges));
+			}
+		}
 		/* Mit der Esc-Taste das Programm beenden */
 		if (KeyEvent.VK_ESCAPE == e.getKeyCode())
 			System.exit(0);
+	}
+
+	private int size(ArrayList<String> tolleListe) {
+		// TODO Auto-generated method stub
+		return 0;
 	}
 
 	@Override
